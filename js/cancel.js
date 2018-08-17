199c2e40d89b5c69730f02454d2944ba2e5d5cca
MH <julsnutini@naver.com>
2018-08-17 17:23:50 +0900

    button changed in contentread.ejs, adjust form changed in contentmodify.ejs, removed useless part of index.ejs

---
+++ js/cancel.js
@@ -0,0 +1,3 @@
+$('#cancel').on('click',function(){
+    window.history.back();
+});
--- routes/board.js
+++ routes/board.js
@@ -2,7 +2,7 @@ var boardfind=function(req,res){
     var database=req.app.get('database');
     console.log('boardfind 호출됨');
     database.BoardModel.find({},function(err,results){
-        res.render('index',{contents:results});
+        res.render('board',{contents:results});
         //res.send(results);//boardLoad.js의 ajax에 results 넘김
     });
 }
