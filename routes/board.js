91300e17dd3b6337ef22b6870c3815b0c9d5d0e9
MH <julsnutini@naver.com>
2018-08-17 14:27:49 +0900

    board with ajax something wrong.

--- routes/board.js
+++ routes/board.js
@@ -1,8 +1,9 @@
 var boardfind=function(req,res){
     var database=req.app.get('database');
-    console.log('bardfind 호출됨');
+    console.log('boardfind 호출됨');
     database.BoardModel.find({},function(err,results){
-        res.render('index',{contents:results});
+        //res.render('index',{contents:results});
+        res.send(results);//boardLoad.js의 ajax에 results 넘김
     });
 }
 
